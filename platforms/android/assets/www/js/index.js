afb713eee4e38dd5ee66bd390415d83abc6f52fa
Leon Eckert <eckertleon@gmail.com>
2015-11-11 23:34:48 -0500

    geolocation works/added folder with process files of all sorts

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,150 +1,68 @@
-// var app = {
-//     // Application Constructor
-//     initialize: function() {
-//         this.bindEvents();
-//     },
-
-
-//     bindEvents: function() {
-//         document.addEventListener('deviceready', this.onDeviceReady, false);
-//         var b = document.getElementById('button');
-//         b.addEventListener("click", this.getLocation, false)
-//     },
-
-    
-
-//     onDeviceReady: function() {
-//         var b = document.getElementById('button');
-//         var o = document.getElementById('output');
-       
-//         b.onclick = function() {
-//             navigator.geolocation.getCurrentPosition(this.onSuccess, this.onError);
-//             o.innerHTML = "i registered the click but navigator doesnt seems to work";
-//         };
-        
-//     },
-
-//     var onSuccess = function(position) {
-//         alert('Latitude: '          + position.coords.latitude          + '\n' +
-//               'Longitude: '         + position.coords.longitude         + '\n' +
-//               'Altitude: '          + position.coords.altitude          + '\n' +
-//               'Accuracy: '          + position.coords.accuracy          + '\n' +
-//               'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-//               'Heading: '           + position.coords.heading           + '\n' +
-//               'Speed: '             + position.coords.speed             + '\n' +
-//               'Timestamp: '         + position.timestamp                + '\n');
-//     },
-
-//     function onError(error) {
-//         alert('code: '    + error.code    + '\n' +
-//               'message: ' + error.message + '\n');
-//     }
-
-
-
-// };
-
-// app.initialize();
-
-
-
-// // onSuccess Callback
-// // This method accepts a Position object, which contains the
-// // current GPS coordinates
-// //
-// // var onSuccess = function(position) {
-// //     alert('Latitude: '          + position.coords.latitude          + '\n' +
-// //           'Longitude: '         + position.coords.longitude         + '\n' +
-// //           'Altitude: '          + position.coords.altitude          + '\n' +
-// //           'Accuracy: '          + position.coords.accuracy          + '\n' +
-// //           'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-// //           'Heading: '           + position.coords.heading           + '\n' +
-// //           'Speed: '             + position.coords.speed             + '\n' +
-// //           'Timestamp: '         + position.timestamp                + '\n');
-// // };
-
-// // // onError Callback receives a PositionError object
-// // //
-// // function onError(error) {
-// //     alert('code: '    + error.code    + '\n' +
-// //           'message: ' + error.message + '\n');
-// // }
-
-// // navigator.geolocation.getCurrentPosition(onSuccess, onError);
-
-
-
-// // var el = document.getElementById('module');
-
-// // el.onclick = function() {
-// //     console.log('Click just happened');
-// // };
-
-
-// // function onSuccess(position) {
-// //     var element = document.getElementById('geolocation');
-// //     element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
-// //                         'Longitude: ' + position.coords.longitude     + '<br />' +
-// //                         '<hr />'      + element.innerHTML;
-// // }
-
-// // // onError Callback receives a PositionError object
-// // //
-// // function onError(error) {
-// //     alert('code: '    + error.code    + '\n' +
-// //           'message: ' + error.message + '\n');
-// // }
-
-// // // Options: throw an error if no update is received every 30 seconds.
-// // //
-// // var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
-
 var app = {
+    
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        console.log("in initialize this is ");
+        console.log(this);
     },
     watchId: null,
 
     // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+    // any listeners created in here will be constantly listening. 
     bindEvents: function() {
+        console.log("in bind events this is ");
+        console.log(this);
         document.addEventListener('deviceready', this.onDeviceReady, false);
         button.addEventListener('click', this.handleClick, false);
     },
-    // deviceready Event Handler
-    //
+    
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+      console.log("in onDeviceReady this is ");
+      console.log(this);
       var options = { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true };
-        function success(position) {
-          output.innerHTML = position.coords.latitude;
-        }
-
-         app.watchId = navigator.geolocation.watchPosition(success,
-                                                  this.onError,
-                                                  options);
+      
+      function success(position) {
+        //this is window
+        output.innerHTML = position.coords.latitude;
+      }
+      app.watchId = navigator.geolocation.watchPosition(success, this.onError, options);
     },
 
-     onError: function(err) {
+
+    onError: function(err) {
+      console.log("in onError this is ");
+      console.log(this);
       alert('you FAILED! DIE!' + err);
     },
 
+
     handleClick: function() {
-      alert("You clicked me");
+      //would "this" refer to button here?
+      console.log("in handleClick this is ");
+      console.log(this);
       app.readLocation();
     },
 
-    readLocation: function() {
-      function onSuccess(position) {
-        output.innerHTML = position.coords.latitude;
-      }
 
+    readLocation: function() {
+      console.log("in readLocation this is ");
+      console.log(this);
+      alert("You clicked me");
+      function onSuccess(position) {
+        console.log("in readLocation>onSuccess this is ");
+        console.log(this);
+        output2.innerHTML = position.coords.latitude;
+      }
+      
       navigator.geolocation.getCurrentPosition(onSuccess, this.onError);
     }
+
+
 };
 
+console.log("outside this is ");
+console.log(this);
 app.initialize();
--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,74 @@
 var app = {
+    
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        /////////////////
+        //this is APP
+        /////////////////
     },
     watchId: null,
 
     // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+    // any listeners created in here will be constantly listening. 
     bindEvents: function() {
+        /////////////////
+        //this is APP
+        /////////////////
         document.addEventListener('deviceready', this.onDeviceReady, false);
         button.addEventListener('click', this.handleClick, false);
     },
-    // deviceready Event Handler
-    //
+    
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+      /////////////////
+      //this is Channel
+      /////////////////
       var options = { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true };
-        function success(position) {
-          output.innerHTML = position.coords.latitude;
-        }
-
-         app.watchId = navigator.geolocation.watchPosition(success,
-                                                  this.onError,
-                                                  options);
+      
+      function success(position) {
+        /////////////////
+        //this is window
+        /////////////////
+        output.innerHTML = position.coords.latitude;
+      }
+      app.watchId = navigator.geolocation.watchPosition(success, this.onError, options);
     },
 
-     onError: function(err) {
+
+    onError: function(err) {
       alert('you FAILED! DIE!' + err);
     },
 
+
     handleClick: function() {
-      alert("You clicked me");
+      /////////////////
+      //this is Button
+      /////////////////
       app.readLocation();
     },
 
-    readLocation: function() {
-      function onSuccess(position) {
-        output.innerHTML = position.coords.latitude;
-      }
 
+    readLocation: function() {
+      /////////////////
+      //this is APP
+      /////////////////
+      alert("You clicked me");
+      function onSuccess(position) {
+        /////////////////
+        //this is Window
+        /////////////////
+        output2.innerHTML = position.coords.latitude;
+      }
+      
       navigator.geolocation.getCurrentPosition(onSuccess, this.onError);
     }
+
+
 };
 
+/////////////////
+//this is window
+/////////////////
 app.initialize();
