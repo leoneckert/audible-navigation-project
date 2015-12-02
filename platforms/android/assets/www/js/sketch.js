e13fafe661eb98839e28bdb6518accde56a34559
Leon Eckert <eckertleon@gmail.com>
2015-12-02 00:22:49 -0500

    8 songs playing simulataneously

--- platforms/android/assets/www/js/sketch.js
+++ platforms/android/assets/www/js/sketch.js
@@ -4,7 +4,7 @@ var cnv;
 // var osc_compass;
 
 function setup() {
-  cnv = createCanvas(200,200);
+  cnv = createCanvas(200,220);
   // osc_gps = new p5.Oscillator();
   // osc_gps.setType('sine');
   // osc_gps.freq(240);
@@ -26,14 +26,26 @@ function draw() {
 
   stroke(0);
   strokeWeight(1);
-  textAlign(CENTER);
-  text("north", width/2, height/2 - 70);
-  text("south", width/2, height/2 + 70);
-  textAlign(RIGHT);
-  text("east", width/2 + 90, height/2);
-  textAlign(LEFT);
-  text("west", width/2 - 90, height/2);
+  // textAlign(CENTER);
+  // text("north", width/2, height/2 - 70);
+  // text("south", width/2, height/2 + 70);
+  // textAlign(RIGHT);
+  // text("east", width/2 + 90, height/2);
+  // textAlign(LEFT);
+  // text("west", width/2 - 90, height/2);
 
+  textAlign(CENTER);
+  text("0°", width/2, height/2 - 70);
+  text("180°", width/2, height/2 + 70);
+  textAlign(RIGHT);
+  text("90°", width/2 + 90, height/2);
+  textAlign(LEFT);
+  text("270°", width/2 - 90, height/2);
+
+
+  textAlign(LEFT);
+  text("black: your heading", width/2 - 90, height/2 + 95);
+  text("red: your destination", width/2 - 90, height/2 + 110);
 
   // if(app.compassAngle < 10 || app.compassAngle > 350){
   //   osc.amp(0.5);
@@ -51,10 +63,16 @@ function draw() {
 
   ellipse(width/2,height/2, 100,100);
   strokeWeight(8);
-  stroke(0, 128);
-  arc(width/2, height/2, 100,100, (app.compassAngle - 2) - 90,(app.compassAngle + 2) - 90);
-  stroke(0, 0, 255, 128);
-  arc(width/2, height/2, 100,100, (app.gpsAngle - 2) - 90,(app.gpsAngle + 2) - 90);
+  // stroke(0, 128);
+  // arc(width/2, height/2, 100,100, (app.compassAngle - 2) - 90,(app.compassAngle + 2) - 90);
+  // stroke(0, 0, 255, 128);
+  // arc(width/2, height/2, 100,100, (app.gpsAngle - 2) - 90,(app.gpsAngle + 2) - 90);
+
+  stroke(0, 0, 0, 200);
+  arc(width/2, height/2, 100,100, (0 - 2) - 90,(0 + 2) - 90);
+  
+  stroke(255, 0, 0, 128);
+  arc(width/2, height/2, 100,100, (app.relativeAngle - 2) - 90,(app.relativeAngle + 2) - 90);
 
 
 }
--- www/js/index.js
+++ www/js/index.js
@@ -11,7 +11,16 @@ var app = {
     watchIdOrientation: null,
     compassAngle: 0,
     gpsAngle: 0,
-    player: null,
+    relativeAngle:0,
+    player0: null,
+    player45: null,
+    player90: null,
+    player135: null,
+    player180: null,
+    player225: null,
+    player270: null,
+    player315: null,
+
 
 
     // Bind Event Listeners
@@ -28,18 +37,69 @@ var app = {
 
         // }).toMaster();
         // var player = new Tone.Player("js/test_sound.m4a",playbackRate: 2).toMaster();
+       
         //the player
-        app.player = new Tone.Player({
-          "url" : "js/yesterday.mp3",
+        app.player0 = new Tone.Player({
+          "url" : "js/000.mp3",
           "loop": true,
           // "playbackRate" : (app.compassAngle - 0) * (7 - (-5)) / (360 - 0) + (- 5),
         }).toMaster();
 
-        
-      
+        app.player45 = new Tone.Player({
+          "url" : "js/045.mp3",
+          "loop": true,
+          // "playbackRate" : (app.compassAngle - 0) * (7 - (-5)) / (360 - 0) + (- 5),
+        }).toMaster();
+
+        app.player90 = new Tone.Player({
+          "url" : "js/090.mp3",
+          "loop": true,
+          // "playbackRate" : (app.compassAngle - 0) * (7 - (-5)) / (360 - 0) + (- 5),
+        }).toMaster();
+
+
+        app.player135 = new Tone.Player({
+          "url" : "js/135.mp3",
+          "loop": true,
+          // "playbackRate" : (app.compassAngle - 0) * (7 - (-5)) / (360 - 0) + (- 5),
+        }).toMaster();
+
+        app.player180 = new Tone.Player({
+          "url" : "js/180.mp3",
+          "loop": true,
+          // "playbackRate" : (app.compassAngle - 0) * (7 - (-5)) / (360 - 0) + (- 5),
+        }).toMaster();
+
+        app.player225 = new Tone.Player({
+          "url" : "js/225.mp3",
+          "loop": true,
+          // "playbackRate" : (app.compassAngle - 0) * (7 - (-5)) / (360 - 0) + (- 5),
+        }).toMaster();
+
+        app.player270 = new Tone.Player({
+          "url" : "js/270.mp3",
+          "loop": true,
+          // "playbackRate" : (app.compassAngle - 0) * (7 - (-5)) / (360 - 0) + (- 5),
+        }).toMaster();
+
+        app.player315 = new Tone.Player({
+          "url" : "js/315.mp3",
+          "loop": true,
+          // "playbackRate" : (app.compassAngle - 0) * (7 - (-5)) / (360 - 0) + (- 5),
+        }).toMaster();
+
+
         Tone.Buffer.onload = function(){
-          print(app.player.playbackRate);
-          app.player.start();
+          // print(app.player0.playbackRate);
+          app.player0.start();
+          app.player45.start();
+          app.player90.start();
+          app.player135.start();
+          app.player180.start();
+          app.player225.start();
+          app.player270.start();
+          app.player315.start();
+          print("play");
         }
 
     },
@@ -59,20 +119,16 @@ var app = {
         //this is window
         /////////////////
         output.innerHTML =  "<b>Latitude:</b> " + position.coords.latitude + "<br><b>Longitude:</b> " + position.coords.longitude;
-                            // "<br>Altitude: " + position.coords.altitude +
-                            // "<br>Accuracy: " + position.coords.accuracy +
-                            // "<br>Altitude Accuracy: " + position.coords.altitudeAccuracy +
-                            // "<br>Heading: " + position.coords.heading +
-                            // "<br>Speed: " + position.coords.speed +
-                            // "<br>Timestamp: " + position.timestamp;
+ 
+
         //in relation to ITP:
         //north: 40.741564, -73.993177
         //east: 40.729109, -73.973264
         //south: 40.717466, -73.994421
         //west: 40.729825, -74.031797
         // between itp and union: 40.732378, -73.991332
-        var dest_lat = 40.729825;
-        var dest_lon = -74.031797;
+        var dest_lat = 40.729109;
+        var dest_lon = -73.973264;
         output1.innerHTML = "<b>Destination bearing:</b> " + String(app.getBearing(position.coords.latitude, position.coords.longitude, dest_lat, dest_lon));
         app.gpsAngle = app.getBearing(position.coords.latitude, position.coords.longitude, dest_lat, dest_lon);
       }
@@ -81,8 +137,310 @@ var app = {
         // output2.innerHTML = "nothing";
         output2.innerHTML = "<b>Compass:</b> " + heading.magneticHeading;
         app.compassAngle = heading.magneticHeading;
-        app.player.playbackRate = (app.compassAngle - 0) * (4 - (0)) / (360 - 0) + (0);
-        // output3.innerHTML = app.angle;
+
+        // app.player0.playbackRate = (app.compassAngle - 0) * (4 - (0)) / (360 - 0) + (0);
+
+
+        if(app.gpsAngle >= app.compassAngle){
+          app.relativeAngle = app.gpsAngle - app.compassAngle;
+        }else{
+          app.relativeAngle = (app.gpsAngle - app.compassAngle)+360;
+        }
+
+        // (app.relativeAngle - in_min) * (out_max - out_min) / (in_max - in_min) + out_min,
+
+        var fade_span = 45;
+
+
+
+        // if(app.relativeAngle >= 360 - fade_span){
+        //   app.player0.set({
+        //     "volume" : (app.relativeAngle - 315) * (0 - -55) / (360 - 315) + -55,
+        //     // "volume" : -55,
+        //   });
+        // }
+
+       
+
+        // if(app.relativeAngle <= fade_span){
+        //   var from = 0;
+        //   var to = 45;
+        //   var left_sound = 0;
+        //   var right_sound = -55;
+          
+        //   app.player0.set({            
+        //     "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (45 - from) + left_sound,
+        //   });
+
+        //   from = 0;
+        //   to = 45;
+        //   left_sound = -55;
+        //   right_sound = 0;
+
+        //   app.player45.set({
+        //     "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (45 - from) + left_sound,
+        //     // "volume" : -55,
+        //   });
+        // }
+
+
+
+        var from = 0;
+        var to = 0;
+        var left_sound = 0;
+        var right_sound = 0;
+        // down
+        // from = secondAngle - 45;
+        // to = secondAngle;
+        // left_sound = 0;
+        // right_sound = -55;
+
+        // // up
+        // from = secondAngle - 45;
+        // to = secondAngle;
+        // left_sound = -55;
+        // right_sound = 0;
+
+        
+
+
+        if(app.relativeAngle >= 0 && app.relativeAngle <= 45){
+          var secondAngle = 45;
+          // 0 down
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = 0;
+          right_sound = -55;
+
+          app.player0.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+
+          //45 up
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = -55;
+          right_sound = 0;
+
+          app.player45.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+        }
+
+
+        if(app.relativeAngle >= 45 && app.relativeAngle <= 90){
+          var secondAngle = 90;
+          //45 down
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = 0;
+          right_sound = -55;
+
+          app.player45.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+
+
+            // "volume" : -55,
+          });
+
+
+
+          //90 up
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = -55;
+          right_sound = 0;
+
+          app.player90.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+
+
+        }
+
+
+        if(app.relativeAngle >= 90 && app.relativeAngle <= 135){
+          var secondAngle = 135;
+
+          //90 down
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = 0;
+          right_sound = -55;
+
+          app.player90.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+          // //135 up
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = -55;
+          right_sound = 0;
+
+          app.player135.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+        }
+
+
+        if(app.relativeAngle >= 135 && app.relativeAngle <= 180){
+          var secondAngle = 180;
+
+          //135 down
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = 0;
+          right_sound = -55;
+
+          app.player135.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+          //180 up
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = -55;
+          right_sound = 0;
+
+          app.player180.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+        }
+
+
+        if(app.relativeAngle >= 180 && app.relativeAngle <= 225){
+          var secondAngle = 225;
+          //180 down
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = 0;
+          right_sound = -55;
+
+          app.player180.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+          //225 up
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = -55;
+          right_sound = 0;
+
+          app.player225.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+        }
+
+
+        if(app.relativeAngle >= 225 && app.relativeAngle <= 270){
+          var secondAngle = 270;
+
+          //225 down
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = 0;
+          right_sound = -55;
+
+          app.player225.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+          //270 up
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = -55;
+          right_sound = 0;
+
+          app.player270.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+        }
+
+
+        if(app.relativeAngle >= 270 && app.relativeAngle <= 315){
+          var secondAngle = 315;
+
+          //270 down
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = 0;
+          right_sound = -55;
+
+          app.player270.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+          //315 up
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = -55;
+          right_sound = 0;
+
+          app.player315.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+        }
+        if(app.relativeAngle >= 315 && app.relativeAngle <= 360){
+          var secondAngle = 360;
+
+          //315 down
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = 0;
+          right_sound = -55;
+
+          app.player315.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+          // 0 up
+          from = secondAngle - 45;
+          to = secondAngle;
+          left_sound = -55;
+          right_sound = 0;
+
+          app.player0.set({
+            "volume" : (app.relativeAngle - from) * (right_sound - left_sound) / (secondAngle - from) + left_sound,
+            // "volume" : -55,
+          });
+
+        }
+
+
+
+
+
+
+
+
+
+       
+       
+
+        output3.innerHTML = "<b>Relative Angle:</b> " + app.relativeAngle;
 
       }
 
@@ -136,104 +494,5 @@ app.initialize();
 
 
 
-
-
-
-
-// //googlemaps:
-
-// var app = {
-    
-//     // Application Constructor
-//     initialize: function() {
-//         this.bindEvents();
-//         /////////////////
-//         //this is APP
-//         /////////////////
-//     },
-//     watchId: null,
-//     watchIdOrientation: null,
-
-//     // Bind Event Listeners
-//     // any listeners created in here will be constantly listening. 
-//     bindEvents: function() {
-//         /////////////////
-//         //this is APP
-//         /////////////////
-//         document.addEventListener('deviceready', this.onDeviceReady, false);
-//         findme.addEventListener('click', this.handleClick, false);
-//     },
-    
-//     // The scope of 'this' is the event. In order to call the 'receivedEvent'
-//     // function, we must explicitly call 'app.receivedEvent(...);'
-//     onDeviceReady: function() {
-      
-//       function success(location) {
-//         var msg = ["Current your location:\n",
-//           "latitude:" + location.latLng.lat,
-//           "longitude:" + location.latLng.lng,
-//           "speed:" + location.speed,
-//           "time:" + location.time,
-//           "bearing:" + location.bearing].join("\n");
-
-//         map.addMarker({
-//           'position': location.latLng,
-//           'title': msg
-//         }, function(marker) {
-//           marker.showInfoWindow();
-//         });
-//       }
-
-    
-//       map.getMyLocation(success, app.onError);
-//     },
-
-
-//     onError: function(err) {
-//       alert('you FAILED! DIE!' + err);
-//     },
-
-
-//     handleClick: function() {
-//       /////////////////
-//       //this is Button
-//       /////////////////
-//       app.readLocation();
-//     },
-
-
-//     readLocation: function() {
-//       /////////////////
-//       //this is APP
-//       /////////////////
-//       function onSuccess(location) {
-//         var msg = ["Current your location:\n",
-//           "latitude:" + location.latLng.lat,
-//           "longitude:" + location.latLng.lng,
-//           "speed:" + location.speed,
-//           "time:" + location.time,
-//           "bearing:" + location.bearing].join("\n");
-
-//         map.addMarker({
-//           'position': location.latLng,
-//           'title': msg
-//         }, function(marker) {
-//           marker.showInfoWindow();
-//         });
-//       }
-
-      
-//       navigator.geolocation.getCurrentPosition(onSuccess, app.onError);
-
-//       map.getMyLocation(onSuccess, app.onError);
-//     }
-
-
-// };
-
-// /////////////////
-// //this is window
-// /////////////////
-// app.initialize();
 
 
